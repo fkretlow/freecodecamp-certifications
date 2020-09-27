7cf7950e6edd2ad69b5cb96987a6f39f51b0640a
Florian Kretlow <floriankretlow@gmx.de>
2020-09-28 01:07:22 +0200

    cosmetics

--- sci-py/budget.py
+++ sci-py/budget.py
@@ -5,18 +5,12 @@ class Category(object):
         self._balance = 0
 
     def deposit(self, amount, description=""):
-        self.ledger.append({
-                "amount": amount,
-                "description": description
-                })
+        self.ledger.append({ "amount": amount, "description": description })
         self._balance += amount
 
     def withdraw(self, amount, description=""):
         if not self.check_funds(amount): return False
-        self.ledger.append({
-                "amount": -amount,
-                "description": description
-                })
+        self.ledger.append({ "amount": -amount, "description": description })
         self._balance -= amount
         return True
 
@@ -25,18 +19,14 @@ class Category(object):
 
     def transfer(self, amount, other):
         if not self.check_funds(amount): return False
-        self.ledger.append({
-                "description": f"Transfer to {other.name}",
-                "amount": -amount
-                })
-        self._balance -= amount
+        self.withdraw(amount, f"Transfer to {other.name}")
         other.deposit(amount, f"Transfer from {self.name}")
         return True
 
     def check_funds(self, amount):
         return amount <= self._balance
 
-    def withdrawals(self):
+    def total_spent(self):
         total = 0
         for entry in self.ledger:
             if entry["amount"] < 0 and not entry["description"].startswith("Transfer"):
@@ -44,8 +34,7 @@ class Category(object):
         return total
 
     def __str__(self):
-        lines = []
-        lines.append(f"{self.name:*^30}")
+        lines = [f"{self.name:*^30}"]
         for entry in self.ledger:
             lines.append(f"{entry['description'][:23]:<23}{entry['amount']:>7.2f}")
         lines.append(f"Total: {self._balance:.2f}")
@@ -60,26 +49,23 @@ def transpose_matrix(matrix):
 
 
 def create_spend_chart(categories):
-    columns = []
-    marks = [f"{x:>3}|" for x in range(100, -1, -10)]
-    columns.append(marks)
+    labelling = [f"{x:>3}|" for x in range(100, -1, -10)]
+    gap = list(" " * 11) + ["-"]
+    columns = [labelling, gap]
 
-    margin = [" " for _ in range(11)] + ["-"]
-    columns.append(margin)
-
-    total_withdrawals = sum(category.withdrawals() for category in categories)
+    total_spent = sum(category.total_spent() for category in categories)
     for category in categories:
         column = []
-        percentage = int(10 * category.withdrawals() / total_withdrawals) * 10
-        empty_rows = (100 - percentage) // 10
-        for _ in range(empty_rows): column.append(" ")
+        percentage = int(10 * category.total_spent() / total_spent) * 10
+        for _ in range((100 - percentage) // 10): column.append(" ")
         for _ in range(percentage // 10 + 1): column.append("o")
         column.append("-")
         column += list(category.name)
         columns.append(column)
-        columns.append(margin)
-        columns.append(margin)
+        columns.append(gap)
+        columns.append(gap)
 
+    # Make all columns equally tall.
     height = max(len(column) for column in columns)
     for column in columns:
         if len(column) < height:
@@ -87,6 +73,7 @@ def create_spend_chart(categories):
             for _ in range(height - len(column)):
                 column.append(" " * width)
 
-    lines = ["Percentage spent by category"]
-    lines += ["".join(line).rstrip() for line in transpose_matrix(columns)]
+    # Remove .rstrip() for the tests on repl.it to succeed.
+    lines = ["Percentage spent by category"] \
+          + ["".join(line).rstrip() for line in transpose_matrix(columns)]
     return "\n".join(lines)
