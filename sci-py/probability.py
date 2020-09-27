5ceefc63d2253acf990238a7da9eb88538b153bc
Florian Kretlow <floriankretlow@gmx.de>
2020-09-27 20:47:27 +0200

    fight inefficiency

--- sci-py/probability.py
+++ sci-py/probability.py
@@ -1,4 +1,9 @@
-import copy
+# The tests on repl.it won't succeed with this implementation because they
+# expect you to actually change `Hat.contents` while drawing. But then you need
+# to create a deep copy of the hat for every run of the experiment, which is
+# terribly inefficient. It's way easier (and better) to just delegate the
+# drawing to `random.sample`
+
 import random
 
 class Hat(object):
@@ -9,23 +14,15 @@ class Hat(object):
                 self.contents.append(color)
 
     def draw(self, count):
-        drawn = []
         if count > len(self.contents):
-            drawn += self.contents
-            self.contents = []
+            return self.contents
         else:
-            for _ in range(count):
-                ball = random.choice(self.contents)
-                drawn.append(ball)
-                self.contents.remove(ball)
-        return drawn
-
+            return random.sample(self.contents, count)
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     num_successes = 0
     for _ in range(num_experiments):
-        new_hat = copy.deepcopy(hat)
-        sample = new_hat.draw(num_balls_drawn)
+        sample = hat.draw(num_balls_drawn)
         if all(sample.count(color) >= count for color, count in expected_balls.items()):
             num_successes += 1
     return num_successes / num_experiments
