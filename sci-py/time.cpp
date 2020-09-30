43a9d2ef08d3a68cd1a79507e135a5b2d0e4f22a
Florian Kretlow <floriankretlow@gmx.de>
2020-09-30 10:29:39 +0200

    flesh out the c++ time calculator just for fun

--- sci-py/time.cpp
+++ sci-py/time.cpp
@@ -1,18 +1,29 @@
 #include <cctype>
+#include <iomanip>
 #include <iostream>
+#include <regex>
 #include <sstream>
 #include <string>
 
 
 class Time {
+    friend std::ostream& operator<<(std::ostream&, const Time&);
+
     public:
-        Time(unsigned _minutes) : minutes(_minutes) { }
-        Time(const std::string& src);
-        Time(const Time& src) : minutes(src.minutes) { }
+        Time() = default;
+        Time(unsigned m) : minutes(m) { }
+        Time(const std::string&);
+        Time(const Time&) = default;
+        ~Time() = default;
 
-        Time operator+(const Time &rhs) const;
+        Time& operator=(const Time& rhs) = default;
 
-        std::string as_string() const;
+        Time operator+(const Time& rhs) const;
+        Time& operator+=(const Time& rhs);
+
+        Time operator-(const Time& rhs) const;
+        Time& operator-=(const Time& rhs);
+
         unsigned get_minutes() const { return minutes; }
         unsigned get_hours() const { return minutes / 60; }
 
@@ -21,48 +32,67 @@ class Time {
 };
 
 
-Time::Time(const std::string& src) {
-    std::string::size_type start, end;
-    for (start = 0, end = 0; isdigit(src[end]); ++end) ;
-    unsigned hours = std::stoul(src.substr(start, end));
-    if (src.size() > 5 && src.substr(6, 8) == "PM") hours += 12;
+Time::Time(const std::string& s) {
+    std::regex re("(\\d{2}):(\\d{2})(?: (AM|PM))?");
+    std::smatch m;
+    std::regex_search(s, m, re);
 
-    for (start = ++end; isdigit(src[end]); ++end) ;
-    minutes = std::stoi(src.substr(start, end)) + 60 * hours;
+    unsigned hours = static_cast<unsigned>(std::stoul(m[1].str()));
+    if (m[3].matched && m[3].str() == "PM" && hours < 12) hours += 12;
+
+    minutes = static_cast<unsigned>(std::stoul(m[2].str())) + hours * 60;
 }
 
 Time Time::operator+(const Time &rhs) const {
     return Time(this->minutes + rhs.minutes);
 }
 
-std::string Time::as_string() const {
-    unsigned hours = get_hours();
-    unsigned _minutes = minutes - hours * 60;
-    std::ostringstream out;
-    if (hours >= 12) {
-        if (hours > 12) hours -= 12;
-        out << hours << ":" << _minutes << " PM";
-    } else {
-        if (hours == 0) hours = 12;
-        out << hours << ":" << _minutes << " AM";
-    }
-    return out.str();
+Time& Time::operator+=(const Time& rhs) {
+    minutes += rhs.minutes;
+    return *this;
 }
 
+Time Time::operator-(const Time &rhs) const {
+    return Time(this->minutes - rhs.minutes);
+}
 
-std::string add_time(const std::string& start,
-                     const std::string& duration)
-{
-    return std::string("Not implemented.");
+Time& Time::operator-=(const Time& rhs) {
+    minutes -= rhs.minutes;
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const Time& t) {
+    unsigned hours = t.get_hours();
+    unsigned minutes = t.minutes - hours * 60;
+    hours %= 24;
+    std::string sign = hours < 12 ? "AM" : "PM";
+    if (hours > 12) hours -= 12;
+
+    std::ios_base::fmtflags f { os.flags() };
+
+    os << std::setfill('0') << std::right;
+    os << std::setw(2) << hours
+       << ":"
+       << std::setw(2) << minutes
+       << " " << sign;
+
+    os.flags(f);
+
+    return os;
 }
 
 
 int main(int argc, char *argv[])
 {
+    if (argc < 3) {
+        std::cerr << "2 parameters required." << std::endl;
+        return -1;
+    }
     std::string start { argv[1] };
-    std::string end { argv[2] };
+    std::string duration { argv[2] };
 
-    const Time result(Time(start) + Time(end));
-    std::cout << result.as_string() << std::endl;
+    Time t = Time(start) + Time(duration);
+    std::cout << t << std::endl;
+
     return 0;
 }
